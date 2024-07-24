5f120d20d0710dc7d17b1f4940844013037db5ad
horadrim-dev <email.alexss@gmail.com>
2024-07-24 13:28:28 +0700

    change env

---
+++ frontend/app/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  urlAddress: '/api',
+  jwtLogin: 'token',
+  jwtRefresh: 'token/refresh',
+  usersUrl: 'users',
+  userChangePasswordPostfix: 'change_password'
+};
